04d7e7ec83a64f05cb2c746aa102a392203f38bd
Ray <ray@sysolve.com>
2018-01-21 22:16:08 +0800

    first commit

---
+++ app/src/main/java/com/sysolve/androidthings/weatherstation/MainActivity.java
@@ -0,0 +1,20 @@
+package com.sysolve.androidthings.weatherstation;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+    private static final String TAG = MainActivity.class.getSimpleName();
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+
+}
